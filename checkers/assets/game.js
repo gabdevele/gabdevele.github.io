679b5efed27c959582590911f36482542cee7ab0
gabdevele <schiraldigabriele@gmail.com>
2025-02-01 08:06:56 +0100

    fixed checkers bugs

--- checkers/assets/game.js
+++ checkers/assets/game.js
@@ -193,7 +193,12 @@ function getAvailableSalti(pos){
     for(let i = 0; i < posSalto.length; i++){
         if(posValid(posSalto[i][0], posSalto[i][1]) && posValid(posInMezzo[i][0], posInMezzo[i][1])){
             let pedinaInMezzo = dame[posInMezzo[i][0]][posInMezzo[i][1]];
-            if((dame[posSalto[i][0]][posSalto[i][1]] == 0) && (pedinaInMezzo != 0 && pedina != pedinaInMezzo)){
+            let currentIsWhite = pedina % 2 === 1;
+            let targetIsWhite = pedinaInMezzo % 2 === 1;
+            
+            if((dame[posSalto[i][0]][posSalto[i][1]] == 0) && 
+               (pedinaInMezzo != 0) && 
+               (currentIsWhite !== targetIsWhite)){
                 posFinali.push([posSalto[i], posInMezzo[i]]);
             }
         }
@@ -337,6 +342,22 @@ const removeHighlights = () => {
         highlight.classList.remove('movable');
     });
 }
+
+const createCrownSvg = (color) => {
+    //TODO:add element to the html and just copy it
+    const svgNS = "http://www.w3.org/2000/svg";
+    const viewBox = "0 0 576 512";
+    const pathData = "M576 136c0 22.09-17.91 40-40 40c-.248 0-.4551-.1266-.7031-.1305l-50.52 277.9C482 468.9 468.8 480 453.3 480H122.7c-15.46 0-28.72-11.06-31.48-26.27L40.71 175.9C40.46 175.9 40.25 176 39.1 176c-22.09 0-40-17.91-40-40S17.91 96 39.1 96s40 17.91 40 40c0 8.998-3.521 16.89-8.537 23.57l89.63 71.7c15.91 12.73 39.5 7.544 48.61-10.68l57.6-115.2C255.1 98.34 247.1 86.34 247.1 72C247.1 49.91 265.9 32 288 32s39.1 17.91 39.1 40c0 14.34-7.963 26.34-19.3 33.4l57.6 115.2c9.111 18.22 32.71 23.4 48.61 10.68l89.63-71.7C499.5 152.9 496 144.1 496 136C496 113.9 513.9 96 536 96S576 113.9 576 136z";
+    const svgElement = document.createElementNS(svgNS, "svg");
+    svgElement.setAttribute("viewBox", viewBox);
+    svgElement.setAttribute("xmlns", svgNS);
+    const pathElement = document.createElementNS(svgNS, "path");
+    pathElement.setAttribute("d", pathData);
+    pathElement.setAttribute("fill", color);
+    svgElement.appendChild(pathElement);
+    return svgElement;
+}
+
 window.onload = () => {
     createTable();
     addPedine();
